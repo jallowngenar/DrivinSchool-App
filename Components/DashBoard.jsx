de17d0a7f38a82cad786476280d8009bc61311d9
cyberdavinci <ebrimaa2ray@gmail.com>
2022-12-28 00:57:31 +0000

    Added some styling, hope you like it

--- Components/DashBoard.jsx
+++ Components/DashBoard.jsx
@@ -1,34 +1,96 @@
-import { View , Text, Image, StyleSheet, ImageBackground, TouchableOpacity} from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ImageBackground,
+  TouchableOpacity,
+  Dimensions,
+  StatusBar,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
-function DashBoard ({navigation}){
-    return(
-        <View style={style.container}>
-            <ImageBackground source={require('../assets/d1.jpeg')} style={{width:400, height: 800, }}>
-            <Text style={{ fontSize: 30,paddingTop: 70, paddingHorizontal:20, fontWeight: '800'}} >Test your driving skill and  get your driving license</Text>
-                 <TouchableOpacity style={style.button} onPress={() => navigation.navigate('Login')}>
-                    <Text style={{color: 'white', fontWeight: '700'}}>Get Started</Text>
-                 </TouchableOpacity>
-
-            </ImageBackground>
-        
+function DashBoard({ navigation }) {
+  const { width, height } = Dimensions.get("window");
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../assets/d1.jpeg")}
+        style={[
+          { width: width, height: height, paddingTop: 30 },
+          styles.dashboardBg,
+        ]}
+      >
+        <View
+          style={[{ width: width, height: height }, styles.viewOverlay]}
+        ></View>
+        <View style={styles.dashboardHeader}>
+          <Text style={styles.headerText}>Test your driving skills</Text>
+          <Text style={styles.headerText}>Get your driving license</Text>
         </View>
-    )
+        <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+          <LinearGradient
+            colors={["#1ACABC", "#073ABBA3"]}
+            style={styles.button}
+          >
+            <Text
+              style={{
+                color: "white",
+                fontWeight: "700",
+                textAlign: "center",
+                fontSize: 22,
+              }}
+            >
+              Get Started
+            </Text>
+          </LinearGradient>
+        </TouchableOpacity>
+      </ImageBackground>
+      <StatusBar />
+    </View>
+  );
 }
 
-
-const style = StyleSheet.create({
-    container:{
-       // backgroundColor: 'red',
-        flex: 1
-        
-    },
-    button:{
-        marginTop: '120%', 
-        marginLeft: '60%', 
-        backgroundColor: 'grey',
-         padding: 10, 
-         borderRadius: 10, 
-         width: 100
-    }
-})
+const styles = StyleSheet.create({
+  container: {
+    // backgroundColor: "red",
+    // backgroundColor: "#00cc00",
+    // backgroundColor: "#000",
+    position: "relative",
+  },
+  dashboardBg: {
+    // paddingTop: 30,
+    justifyContent: "space-between",
+    alignItems: "center",
+    position: "absolute",
+  },
+  dashboardHeader: {
+    marginTop: 30,
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#fff",
+    textTransform: "uppercase",
+  },
+  button: {
+    // marginTop: "120%",
+    // marginLeft: "60%",
+    // backgroundColor: "grey",
+    // padding: 10,
+    marginBottom: 40,
+    borderRadius: 10,
+    paddingVertical: 10,
+    width: 300,
+  },
+  viewOverlay: {
+    backgroundColor: "#000",
+    position: "absolute",
+    opacity: 0.4,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    // top: 20,
+  },
+});
 export default DashBoard;
--- Components/Login.js
+++ Components/Login.js
@@ -1,33 +1,50 @@
-import {Text,TextInput, TouchableOpacity, View} from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
-function Login({navigation}) {
-    return(
-        <>
-        <View style={{marginBottom: 50}}>
-        <Text style={{marginTop: 75, marginLeft: 50, fontSize: 35}}>Create an Account</Text>
+function Login({ navigation }) {
+  return (
+    <>
+      <View style={{ marginBottom: 50 }}>
+        <Text style={{ marginTop: 75, textAlign: "center", fontSize: 35 }}>
+          Create an Account
+        </Text>
 
-        
-    <View style={{marginHorizontal: 40, marginTop: 40}}>
-            
-        <View>
-        <Text>User Name</Text>
-       <TextInput style={{borderBottomWidth:0.5,padding:5}} placeholder="user@gmail.com" />
-       </View>
+        <View style={{ marginHorizontal: 40, marginTop: 40 }}>
+          <View>
+            <Text>User Name</Text>
+            <TextInput
+              style={{ borderBottomWidth: 0.5, padding: 5 }}
+              placeholder="user@gmail.com"
+            />
+          </View>
 
-        <View style={{marginTop: 40}}>
-       <Text>Password</Text>
-       <TextInput style={{borderBottomWidth:0.5,padding:5}} placeholder="Enter Password" />
-       </View>
-       </View>
+          <View style={{ marginTop: 40 }}>
+            <Text>Password</Text>
+            <TextInput
+              style={{ borderBottomWidth: 0.5, padding: 5 }}
+              placeholder="Enter Password"
+              secureTextEntry
+            />
+          </View>
+        </View>
 
-       {/*  */}
-       <TouchableOpacity style={{backgroundColor: 'grey', padding: 10 , width: '30%', alignSelf: 'center', marginTop: '50%', borderRadius: 5}}
-               onPress={() => navigation.navigate('Services')} >
-        <Text style={{marginLeft: 20, color: 'white', fontWeight: '900'}}>Register</Text>
-       </TouchableOpacity>
-       </View>
-
-        </>
-    )
+        {/*  */}
+        <TouchableOpacity
+          style={{
+            backgroundColor: "grey",
+            padding: 10,
+            width: "30%",
+            alignSelf: "center",
+            marginTop: "50%",
+            borderRadius: 5,
+          }}
+          onPress={() => navigation.navigate("Services")}
+        >
+          <Text style={{ marginLeft: 20, color: "white", fontWeight: "900" }}>
+            Register
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </>
+  );
 }
 export default Login;
